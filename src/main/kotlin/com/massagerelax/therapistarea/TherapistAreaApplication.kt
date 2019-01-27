6fc8b3dd6f90f44b8ec21938d54b2db822043bea
Martin Muller <martin.muller@alfresco.com>
2019-01-27 17:19:39 +0000

    init

---
+++ src/main/kotlin/com/massagerelax/therapistarea/TherapistAreaApplication.kt
@@ -0,0 +1,12 @@
+package com.massagerelax.therapistarea
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TherapistAreaApplication
+
+fun main(args: Array<String>) {
+	runApplication<TherapistAreaApplication>(*args)
+}
+
